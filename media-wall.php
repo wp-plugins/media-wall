d513a24b4d673cbf398ec8ed3d30b218ec5020ac
grabimo <grabimo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 21:11:27 +0000

    support https
    git-svn-id: https://plugins.svn.wordpress.org/media-wall/trunk@947092 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- media-wall.php
+++ media-wall.php
@@ -3,7 +3,7 @@
 Plugin Name: Media Wall
 Plugin URI: http://www.grabimo.com
 Description: Enable yourself and your website visitors to post video, audio, photo, and text on your website. You manage which media to display on your site.
-Version: 1.0.1
+Version: 1.1.0
 Author: Grabimo
 Author URI: http://www.grabimo.com
 License: GPLv2 or later
@@ -59,7 +59,7 @@ if (is_admin()){ // admin actions and filters
 // action function to add a new submenu under Settings
 function media_wall_admin_menu() {
 	// Add a new submenu under Settings
-	add_options_page('Media Wall', '<img style="position:relative;top:4px" src="http://developer.grabimo.com/download/grabimo16x16.png"/>&nbsp;Media Wall', 'manage_options', 'media_wall', 'media_wall_options_page');
+	add_options_page('Media Wall', '<img style="position:relative;top:4px" src="//www.grabimo.com/download/grabimo16x16.png"/>&nbsp;Media Wall', 'manage_options', 'media_wall', 'media_wall_options_page');
 }
 
 // Use Settings API to whitelist options
@@ -69,7 +69,7 @@ function media_wall_settings_api_init() {
 
 // Build array of links for rendering in installed plugins list
 function media_wall_admin_plugin_actions($links) {
-	$settings_link = '<img style="position:relative;top:4px" src="http://developer.grabimo.com/download/grabimo16x16.png"/>&nbsp;<a href="'. get_admin_url(null, 'options-general.php?page=media_wall') .'">Settings</a>';
+	$settings_link = '<img style="position:relative;top:4px" src="//www.grabimo.com/download/grabimo16x16.png"/>&nbsp;<a href="'. get_admin_url(null, 'options-general.php?page=media_wall') .'">Settings</a>';
 	array_unshift($links, $settings_link);
 	
 	return $links;
@@ -78,12 +78,12 @@ function media_wall_admin_plugin_actions($links) {
 // --- add javascrit and CSS file on webpage head ------------
 function media_wall_css_js_files() {	
 	// add Javscript
-	wp_enqueue_script( 'multimedia-feedback-js-file', 'http://developer.grabimo.com/download/mf.js' );	
+	wp_enqueue_script( 'multimedia-feedback-js-file', '//www.grabimo.com/download/mf.js' );	
 	wp_enqueue_script( 'media-wall-iframe-resize-js-file', plugins_url( 'iframeResizer.min.js', __FILE__ ), array(), '1.0.0', true );
 	wp_enqueue_script( 'media-wall-js-file', plugins_url( 'media-wall.js', __FILE__ ), array(), '1.0.0', true );
 	
 	// CSS file
-	wp_enqueue_style( 'multimedia-feedback-css-file', 'http://developer.grabimo.com/download/mf.css' );	
+	wp_enqueue_style( 'multimedia-feedback-css-file', '//www.grabimo.com/download/mf.css' );	
 }
 add_action( 'wp_enqueue_scripts', 'media_wall_css_js_files');
 
@@ -94,7 +94,7 @@ function media_wall_short_code() {
 	$media_wall_font_family			= $media_wall_plugin_option_array[ 'font_family' ];
 	$media_wall_enable_post			= $media_wall_plugin_option_array[ 'enable_post' ];	
 	$html = '<div style="width:100%;position:relative">';
-	$html = $html . '<iframe id="grabimo_media_wall" src="http://www.grabimo.com/app/bizGigDoc.html?alias=' . $media_wall_business_alias;
+	$html = $html . '<iframe id="grabimo_media_wall" src="//www.grabimo.com/app/bizGigDoc.html?alias=' . $media_wall_business_alias;
 	$html = $html . '&post=' . $media_wall_enable_post . '&font=' . rawurlencode($media_wall_font_family);
 	$html = $html . '" scrolling="no" width="100%" style="border: none;"></iframe>';
 	if ($media_wall_enable_post) {
@@ -130,7 +130,7 @@ function media_wall_options_page() {
 		<tr valign="top">
 		<td style="width:250px"><label for="media_wall_business_alias">Set business alias</label></td>
 		<td><input maxlength="30" size="25" type="text" name="media_wall_plugin_options[business_alias]" value="<?php echo esc_html( $media_wall_business_alias ); ?>" />
-			<p class="description">To create an Alias for your website, sign up at <a href="http://www.grabimo.com">http://www.grabimo.com</a></td>
+			<p class="description">To create an Alias for your website, sign up at <a href="https://www.grabimo.com">https://www.grabimo.com</a></td>
 		</tr>
 		
 		<tr valign="top">
